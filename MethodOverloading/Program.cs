5b4205d89a41e98570ab8eea0656fe63a48aa598
Austin Okyere <okyereaustin@gmail.com>
2025-10-14 15:33:25 -0600

    Completed Methodoverloading exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(5, 5, true));
+        }
+
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static double Add(double numOne, double numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static string Add(int numOne, int numTwo, bool isTrue)
+        {
+            var sum = numOne + numTwo;
+            if (isTrue && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+
+            return $"{sum} dollars";
         }
     }
 }
